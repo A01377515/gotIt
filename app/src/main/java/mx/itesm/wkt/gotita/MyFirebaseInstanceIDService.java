622e114a3a858ac9aa0aac3f927cdfd776da4f66
kevinwkt <kevinwkt97@outlook.com>
2018-05-02 01:06:25 -0500

    update

---
+++ app/src/main/java/mx/itesm/wkt/gotita/MyFirebaseInstanceIDService.java
@@ -0,0 +1,23 @@
+package mx.itesm.wkt.gotita;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Send any registration to your app's servers.
+    }
+}
